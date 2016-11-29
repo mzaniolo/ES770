075b1daf88edbc17bdbadfd00a3ffdc3f10f681b
Lucas <lucasemc19@gmail.com>
2016-11-29 02:43:38 -0200

    Update pwm_motor.h

--- Carrinho/Sources/pwm_motor.h
+++ Carrinho/Sources/pwm_motor.h
@@ -1,16 +1,33 @@
-/*
- * pwm_motor.h
- *
- *  Created on: 11/10/2016
- *      Author: ES670A
- */
+/* ***************************************************************** */
+/* File name:         pwm_mtotor.h                                  */
+/* File description: Contain functions for activation and changing  */
+/*                   velocity of motors  		           */
+/* Author name:      Lucas_C e Matheus_Z                            */
+/* Creation date:    25/10/2016                                     */
+/* ***************************************************************** */
 
 #ifndef SOURCES_PWM_MOTOR_H_
 #define SOURCES_PWM_MOTOR_H_
 
-void timer_initTPMx_AsPWM_Motor(int iMotorPercent);
+/* ************************************************** */
+/* Method name: 	   motor_initTPM1AsPWM              */
+/* Method description:  configure Timer1 to act as PWM */
+/*			                and initialize motor with     */
+/*			                desired velocity              */
+/* Input params:	      int iMotorPercent             */
+/* Outpu params:	        n/a 			                  */
+/* ************************************************** */
+void motor_initTPMx_AsPWM_Motor(int iMotorPercent);
 
+/* ************************************************** */
+/* Method name: 	  motor_change_percentage           */
+/* Method description:   change PWM % sent to specific*/
+/*			                  motor			                 */
+/* Input params:	  int iPercent, int iMotorNum      */
+/* Outpu params:	   n/a 			                       */
+/* ************************************************** */
 void motor_change_percentage(int iPercent, int iMotorNum);
+
 
 
 
