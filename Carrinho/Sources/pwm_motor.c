7fd079567ebdec79c740b4a54cb2ee46690ebbf8
Lucas <lucasemc19@gmail.com>
2016-11-29 02:38:34 -0200

    Update pwm_motor.c

--- Carrinho/Sources/pwm_motor.c
+++ Carrinho/Sources/pwm_motor.c
@@ -1,7 +1,10 @@
-/*
- * pwm_motor.c
- */
-
+/* ***************************************************************** */
+/* File name:         pwm_mtotor.c                                  */
+/* File description: Contain functions for activation and changing  */
+/*                   velocity of motors  		           */
+/* Author name:      Lucas_C e Matheus_Z                            */
+/* Creation date:    25/10/2016                                     */
+/* ***************************************************************** */
 #include "es670_peripheral_board.h"
 #include "pwm_motor.h"
 
@@ -9,12 +12,14 @@
 
 
 /* ************************************************** */
-/* Method name: 	   timer_initTPM1AsPWM            */
-/* Method description: configure Timer1 to act as PWM */
-/* Input params:	   n/a 							  */
-/* Outpu params:	   n/a 							  */
+/* Method name: 	   timer_initTPM1AsPWM        */
+/* Method description:  configure Timer1 to act as PWM */
+/*			and initialize motor with     */
+/*			desired velocity              */
+/* Input params:	   int iMotorPercent          */
+/* Outpu params:	   n/a 			      */
 /* ************************************************** */
-void timer_initTPMx_AsPWM_Motor(int iMotorPercent) {
+void motor_initTPMx_AsPWM_Motor(int iMotorPercent) {
 	/* un-gate port clock*/
 		SIM_SCGC5 |= SIM_SCGC5_PORTA(CGC_CLOCK_ENABLED);
 		SIM_SCGC5 |= SIM_SCGC5_PORTE(CGC_CLOCK_ENABLED);
@@ -40,25 +45,28 @@ void timer_initTPMx_AsPWM_Motor(int iMotorPercent) {
 		while(!(TPM0_C2SC&(TPM_CnSC_MSB(0b1) | TPM_CnSC_ELSB(0b1) )));
 
 
-		//int valuem = (int)((TPM0_MOD/100) * iMotorPercent);
-
-		// 0.50% a 2.30%
 		TPM0_C1V = iMotorPercent*50; //PWM %
 
-		TPM0_C2V = iMotorPercent*50; //TPM_CnV_VAL(valuem); //PWM %
+		TPM0_C2V = iMotorPercent*50; //PWM %
 
 		TPM0_SC |= TPM_SC_CMOD(1);   //LPTPM Counter increments on every LPTPM counter clock
-		TPM0_MOD = 5000;//1875
+		TPM0_MOD = 5000; //chosen module
 
 }
 
+/* ************************************************** */
+/* Method name: 	  motor_change_percentage     */
+/* Method description:   change PWM % sent to specific*/
+/*			motor			      */
+/* Input params:	  int iPercent, int iMotorNum */
+/* Outpu params:	   n/a 			      */
+/* ************************************************** */
 void motor_change_percentage(int iPercent, int iMotorNum) {
-	//int value = (int)((TPM0_MOD/100) * iPercent);
-
-	if (iMotorNum == 1) {//motor direita
+	
+	if (iMotorNum == 1) {//motor right
 		TPM0_C1V = iPercent*50; //PWM %
 	}
-	else if (iMotorNum == 2) {//motor esquerda
+	else if (iMotorNum == 2) {//motor left
 		TPM0_C2V = iPercent*50; //PWM %
 	}
 }
