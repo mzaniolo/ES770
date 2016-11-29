436c9ba7b06909598672a4024efa5f7818d946df
Lucas <lucasemc19@gmail.com>
2016-11-29 02:13:49 -0200

    Update autoteste.h

--- Carrinho/Sources/autoteste.h
+++ Carrinho/Sources/autoteste.h
@@ -1,15 +1,30 @@
-/*
- * autoteste.h
- *
- *  Created on: 05/11/2016
- *      Author: ES670A
- */
+
+/* ***************************************************************** */
+/* File name:        autoteste.h                                    */
+/* File description: Contém funções utilizadas para o autoteste     */
+/*                   das difrentes partes de harware do sistema     */
+/* Author name:      Lucas_C e Matheus_Z                            */
+/* Creation date:    05/11/2016                                     */
+/* ***************************************************************** */
 
 #ifndef SOURCES_AUTOTESTE_H_
 #define SOURCES_AUTOTESTE_H_
 
-
+/* ************************************************ */
+/* Method name:        autoteste_leds               */
+/* Method description: realiza o teste dos LEDs do  */
+/*			sistema			    */
+/* Input params:       n/a			    */
+/* Output params:      n/a                         */
+/* ************************************************ */
+void autoteste_leds(void);
+/* ************************************************ */
+/* Method name:        autoteste_leds               */
+/* Method description: realiza o teste dos motores  */
+/*			do sistema		    */
+/* Input params:       n/a			    */
+/* Output params:      n/a                         */
+/* ************************************************ */
 void autoteste_motor(void);
-
 
 #endif /* SOURCES_AUTOTESTE_H_ */
