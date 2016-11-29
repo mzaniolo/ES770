836cd60487e97075b6eb7b0f9d10513ca0494f92
Lucas <lucasemc19@gmail.com>
2016-11-29 02:24:52 -0200

    Update encoder_hal.h

--- Carrinho/Sources/encoder_hal.h
+++ Carrinho/Sources/encoder_hal.h
@@ -1,14 +1,28 @@
-/*
- * encoder_hal.h
- *
- *  Created on: 25/10/2016
- *      Author: ES670A
- */
+/* ***************************************************************** */
+/* File name:       encoder.h                                       */
+/* File description: Contain functions for activation and reading  */
+/*                   of the system encoders		           */
+/* Author name:      Lucas_C e Matheus_Z                            */
+/* Creation date:    25/10/2016                                     */
+/* ***************************************************************** */
 
 #ifndef SOURCES_ENCODER_HAL_H_
 #define SOURCES_ENCODER_HAL_H_
-
+/* ************************************************ */
+/* Method name:        encoder_init                 */
+/* Method description: Initialize the encoder        */
+/* Input params:       n/a                          */
+/* Output params:      n/a                          */
+/* ************************************************ */
 void encoder_init(void);
 
+/* ************************************************ */
+/* Method name:        encoder_read                 */
+/* Method description: Reads the encoder value  of  */
+/*			                chosen motor		            */
+/* Input params:       int iMotor                   */
+/* Output params:      n/a                          */
+/* ************************************************ */
+int encoder_read(int iMotor);
 
 #endif /* SOURCES_ENCODER_HAL_H_ */
